2cc7c686ffca991562feb1b7cf4e98c53fa2a40e
Mariela Kasovska <mariela.kasovska@gmail.com>
2016-10-10 17:56:37 +0300

    Refactor the app entry point

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import { configureStore } from './configureStore';
+import Root from './components/Root';
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
