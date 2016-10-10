e5c410c82b8accec1882313aafeb9fb307fd5842
Mariela Kasovska <mariela.kasovska@gmail.com>
2016-10-10 13:49:47 +0300

    Persist state to local storage

--- src/api/localStorage.js
+++ src/api/localStorage.js
@@ -1,3 +1,23 @@
 export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
 
+    // if there is no serialized state in the localStorage return undefined in order for the reducer to set the correct state
+    if(serializedState === null) {
+      return undefined;
+    }
+
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
 };
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch(err) {
+    return undefined;
+  }
+}
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,19 @@
 import { createStore } from 'redux';
 import todoApp from './reducers/rootReducer';
-import { loadState } from './api/localStorage';
+import { loadState, saveState } from './api/localStorage';
 
 // pass persisted state to the reducer to init the app; if some part of the persisted state is undefined, the reducer returns the default state
 const persistedState = loadState();
 
 export const configureStore = () => {
-  return createStore(todoApp, persistedState);
-}
+  let store = createStore(todoApp, persistedState);
+
+  // save only the todos array, so the visibility filter is the default one after page react-hot-loader
+  store.subscribe(() => {
+    saveState({
+      todos: store.getState().todos
+    });
+  });
+
+  return store;
+};
